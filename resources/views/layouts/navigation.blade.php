cffe9121eb00d1a4be1eae166dd5a101a1a4b3d4
Ronald <ronalddantasrodrigues18@gmail.com>
2023-06-01 20:32:05 -0300

    .

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="img/Logo.png" alt="">
+                        {{-- <img src="img/Logo.png" alt=""> --}}
                     </a>
                 </div>
 
