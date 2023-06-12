0dec14c3fd0c354603295a7a9ea763ec01847986
Vinicius <vine.goncalo123@gmail.com>
2023-06-12 00:27:14 -0300

    aa

--- resources/views/launch.blade.php
+++ resources/views/launch.blade.php
@@ -1,21 +1,21 @@
 
 <x-app-layout>
     <main class="flex justify-center w-full">
-        <div class="flex flex-col w-96 mt-48 bg-violet-200 p-10 rounded-xl">
-            <label for="number">Valor da Transação:</label>
-            <input type="number" name="price" step="0.01" placeholder="R$ 0,00">
-            <label for="date">Data da Transação:</label>
-            <input type="date" name="date" id="date">
+        <div class="flex flex-col w-96 mt-40 bg-violet-200 p-8 rounded-xl">
+            <label for="number" class="text-violet-700 mb-1">Valor da Transação:</label>
+            <input type="number" name="price" step="0.01" placeholder="R$ 0,00" class=" border-purple-500 mb-1 rounded-xl p-2">
+            <label for="date" class="text-violet-700">Data da Transação:</label>
+            <input type="date" name="date" id="date" class=" border-purple-500 mb-2 rounded-xl p-2 text-center ">
             <div>
-                <label for="number">Categoria:</label>
-                <select name="" id="">
+                <label for="number" class="text-violet-700 mr-2">Categoria:</label>
+                <select name="" id="" class="rounded-xl pr-16 text-center mb-3">
                     <option value="">escolha a categoria...</option>
                     @foreach ($categories as $category)
                     <option value="{{$category->id}}">{{$category->name}}</option>
                     @endforeach
                 </select>
             </div>
-                <a href="/categories/create">Adicionar Categoria</a>
+                <a href="/categories/create" class="bg-indigo-600 rounded-lg  w-6/12 flex items justify-center">Adicionar Categoria</a>
             <label class="relative inline-flex items-center mb-4 cursor-pointer">
                 <input type="checkbox" value="" name="type" class="sr-only peer">
                 <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
