06b66b4e3b5a4fde47753db3147c2d5bca9bb8f5
Vinicius <vine.goncalo123@gmail.com>
2023-06-15 00:57:52 -0300

    att

--- resources/views/launch.blade.php
+++ resources/views/launch.blade.php
@@ -15,13 +15,13 @@
                     @endforeach
                 </select>
             </div>
-                <a href="/categories/create" class="bg-indigo-600 rounded-lg  w-6/12 flex items justify-center">Adicionar Categoria</a>
+                <a href="/categories/create" class="bg-violet-400 rounded-lg px-8 py-2 mb-3 flex items justify-center ease-out duration-200 hover:bg-violet-700 hover:text-sky-50 ">Adicionar Categoria</a>
             <label class="relative inline-flex items-center mb-4 cursor-pointer">
                 <input type="checkbox" value="" name="type" class="sr-only peer">
-                <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <div class="w-11 h-6 bg-gray-300 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                 <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Recorrente</span>
             </label>
-            <input class=" top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit" onclick="transformarVirgulaPonto()" value="Registrar">
+            <input type="submit" value="Cadastrar-se" class="bg-violet-400 rounded-lg px-8 py-2 flex items justify-center ease-out duration-200 hover:bg-violet-700 hover:text-sky-50 ">
         </div>
     </main>
 </x-app-layout>
